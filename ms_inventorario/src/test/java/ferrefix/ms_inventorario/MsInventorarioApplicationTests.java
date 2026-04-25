60ac5435ca0a2bebaa5b4e08f4ca3b8e0f0090a1
arcatalanm <arielca329@gmail.com>
2026-04-25 02:33:25 -0400

    chore(setup): inicializar arquitectura base del proyecto - Se integran las dependencias de Lombok, Spring Data JPA, MySQL, Validation y Spring Web. - Se define la estructura en capas creando los directorios: `model`, `controller`, `repository`, `service`, `dto` y `util`. - Este setup complementa el entorno principal de trabajo.

---
+++ ms_inventorario/src/test/java/ferrefix/ms_inventorario/MsInventorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package ferrefix.ms_inventorario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
