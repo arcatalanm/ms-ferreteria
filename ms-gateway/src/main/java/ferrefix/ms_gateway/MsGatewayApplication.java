fd6baa93434078dafbf3b52dafc9a97b4c40fa6b
lecheconplatano <ar.catalanm@duocuc.cl>
2026-05-03 19:53:34 -0400

    config(gateway): actualización menor de configuración y enrutamiento

--- ms-gateway/src/main/java/ferrefix/ms_gateway/MsGatewayApplication.java
+++ ms-gateway/src/main/java/ferrefix/ms_gateway/MsGatewayApplication.java
@@ -2,6 +2,8 @@ package ferrefix.ms_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MsGatewayApplication {
